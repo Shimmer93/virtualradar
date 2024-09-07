9c008601bffafb4197799d3bc9bdaae8ba2e84e0
Shimmer93 <zpengac@connect.ust.hk>
2024-09-07 12:43:26 +0800

    update processing code

--- ProcessingScripts/main.py
+++ ProcessingScripts/main.py
@@ -10,10 +10,11 @@ import argparse
 import datetime
 
 class TCPServer:
-    def __init__(self, host, port):
+    def __init__(self, host, port, timeout=120):
         self.host = host
         self.port = port
         self.tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.timeout = timeout
         self.conn = None
         self.addr = None
 
@@ -25,7 +26,7 @@ class TCPServer:
         
     def receive_data(self, datalengthbyte):
         data = self.conn.recv(datalengthbyte)
-        self.conn.settimeout(60)
+        self.conn.settimeout(self.timeout)
         return data
     
     def close_server(self):
@@ -34,7 +35,7 @@ class TCPServer:
     
 class RadarSignalProcessor:
     def __init__(self, 
-                 num_chirps=1, 
+                 num_chirps=48, 
                  num_samples=300, 
                  num_antennas=4,
                  center_freq=60.5e9, 
@@ -62,31 +63,69 @@ class RadarSignalProcessor:
         self.cfar_num_guard = cfar_num_guard
         self.cfar_threshold = cfar_num_train * (cfar_false_alarm_rate ** (-1 / cfar_num_train) - 1)
 
-    def cfar_algorithm(self, data):
-        assert data.shape[1] > self.cfar_num_train + self.cfar_num_guard, 'Signal length is too short'
+        self.win_range = hann(self.num_samples)
+        self.win_doppler = hann(self.num_chirps)
+        self.use_window = True
 
+    def range_fft(self, data):
+        if self.use_window:
+            data = data * self.win_range[np.newaxis, :, np.newaxis]
+        range_fft = np.fft.fft(data, axis=1)
+        return range_fft
+
+    def doppler_fft(self, data):
+        if self.use_window:
+            data = data * self.win_doppler[:, np.newaxis, np.newaxis]
+        doppler_fft = np.fft.fftshift(np.fft.fft(data, axis=0), axes=0)
+        return doppler_fft
+
+    def cfar(self, data):
+        # data: num_chirps x num_samples
+        assert data.shape[1] > self.cfar_num_train + self.cfar_num_guard, 'Signal length is too short'
         cfar_map = np.zeros_like(data)
-        for chirp in range(data.shape[0]):
-            for antenna in range(data.shape[2]):
-                for i in range(self.cfar_num_train + self.cfar_num_guard, data.shape[1] - self.cfar_num_train - self.cfar_num_guard):
-                    training_cells = np.concatenate([data[chirp, i - self.cfar_num_train - self.cfar_num_guard:i - self.cfar_num_guard, antenna], data[chirp, i + self.cfar_num_guard + 1:i + self.cfar_num_guard + self.cfar_num_train + 1, antenna]])
-                    noise_level = np.mean(training_cells)
-                    threshold = self.cfar_threshold * noise_level
-                    if data[chirp, i, antenna] > threshold:
-                        cfar_map[chirp, i, antenna] = data[chirp, i, antenna]
+        for i in range(data.shape[0]):
+            for j in range(self.cfar_num_train + self.cfar_num_guard, data.shape[1] - self.cfar_num_train - self.cfar_num_guard):
+                training_cells = np.concatenate([data[i, j - self.cfar_num_train - self.cfar_num_guard:j - self.cfar_num_guard], data[i, j + self.cfar_num_guard + 1:j + self.cfar_num_guard + self.cfar_num_train + 1]])
+                noise_level = np.mean(training_cells)
+                threshold = self.cfar_threshold * noise_level
+                if data[i, j] > threshold:
+                    cfar_map[i, j] = data[i, j]
 
         return cfar_map
+
+    # def cfar_algorithm(self, data):
+    #     assert data.shape[1] > self.cfar_num_train + self.cfar_num_guard, 'Signal length is too short'
+
+    #     cfar_map = np.zeros_like(data)
+    #     for chirp in range(data.shape[0]):
+    #         for antenna in range(data.shape[2]):
+    #             for i in range(self.cfar_num_train + self.cfar_num_guard, data.shape[1] - self.cfar_num_train - self.cfar_num_guard):
+    #                 training_cells = np.concatenate([data[chirp, i - self.cfar_num_train - self.cfar_num_guard:i - self.cfar_num_guard, antenna], data[chirp, i + self.cfar_num_guard + 1:i + self.cfar_num_guard + self.cfar_num_train + 1, antenna]])
+    #                 noise_level = np.mean(training_cells)
+    #                 threshold = self.cfar_threshold * noise_level
+    #                 if data[chirp, i, antenna] > threshold:
+    #                     cfar_map[chirp, i, antenna] = data[chirp, i, antenna]
+
+    #     return cfar_map
     
     def process_signal(self, data):
         # data: num_chirps x num_samples x num_antennas
-        # assert data.shape == (self.num_chirps_process, self.num_samples, self.num_antennas), f'Data shape is incorrect: {data.shape}'
+        # assert data.shape == (self.num_chirp, self.num_samples, self.num_antennas), f'Data shape is incorrect: {data.shape}'
 
         # Apply FFT to convert time domain to frequency domain
-        range_fft = np.fft.fft(data, axis=1)
-        doppler_fft = np.fft.fft(range_fft, axis=0)
+        print(data)
+        range_fft = self.range_fft(data)
+        doppler_fft = self.doppler_fft(range_fft)
+
+        filename = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S.png')
+        filename = f'range_fft_{filename}'
+        abs_doppler_fft = np.abs(doppler_fft)
+        abs_doppler_fft = abs_doppler_fft / np.max(abs_doppler_fft)
 
         # Apply CFAR to range-doppler map
-        cfar_map = self.cfar_algorithm(np.abs(doppler_fft))
+        range_doppler_map = np.abs(doppler_fft).sum(axis=2)
+        plot_range_doppler(range_doppler_map, filename=filename)
+        cfar_map = self.cfar(range_doppler_map)
 
         # Detect peaks in CFAR map
         detections = np.argwhere(cfar_map > 0)
@@ -94,7 +133,7 @@ class RadarSignalProcessor:
         # Calculate range, velocity, and angle
         points = []
         for detection in detections:
-            doppler_bin, range_bin, antenna = detection
+            doppler_bin, range_bin = detection
 
             # Calculate range and doppler speed
             range = self.range_res * range_bin
@@ -106,7 +145,7 @@ class RadarSignalProcessor:
             elevation_angle = np.arcsin(phase_diffs[2] / (2 * np.pi * self.dist_antenna)) if self.num_antennas > 2 else 0
 
             # Calculate intensity
-            intensity = cfar_map[doppler_bin, range_bin, antenna] 
+            intensity = cfar_map[doppler_bin, range_bin] 
             intensity = 10 * np.log10(intensity) if intensity > 0 else 0 # Convert to dB
 
             # Convert to Cartesian coordinates
@@ -117,12 +156,27 @@ class RadarSignalProcessor:
             points.append(np.array([x, y, z, doppler_speed, intensity]))
 
         # Convert to numpy array
-        points = np.stack(points)
-        print(points.shape)
+        if len(points) > 0:
+            points = np.stack(points)
+            print(points.shape)
+        else:
+            points = None
+            print('No points detected')
 
         return points
     
+def plot_range_doppler(doppler_fft, filename='range_doppler.png'):
+    if doppler_fft is None:
+        return
+    fig, ax = plt.subplots()
+    ax.imshow(np.abs(doppler_fft), aspect='auto', origin='lower', extent=[0, doppler_fft.shape[1], 0, doppler_fft.shape[0]])
+    ax.set_xlabel('Range')
+    ax.set_ylabel('Doppler')
+    fig.savefig(filename)
+
 def plot_points(points, filename='points.png'):
+    if points is None:
+        return
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.scatter(points[:, 0], points[:, 1], points[:, 2])
@@ -136,8 +190,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--host', type=str, default='localhost', help='Host IP address')
     parser.add_argument('--port', type=int, default=55000, help='Port number')
-    parser.add_argument('--num_chirps', type=int, default=1, help='Number of chirps per frame')
-    parser.add_argument('--num_chirps_process', type=int, default=48, help='Number of chirps to process')
+    parser.add_argument('--num_chirps_per_frame', type=int, default=1, help='Number of chirps per frame')
+    parser.add_argument('--num_chirps', type=int, default=48, help='Number of chirps to process')
     parser.add_argument('--num_samples', type=int, default=256, help='Number of samples')
     parser.add_argument('--num_antennas', type=int, default=4, help='Number of antennas')
     parser.add_argument('--center_freq', type=float, default=60.5e9, help='Center frequency')
@@ -155,7 +209,7 @@ def main():
 
     # Initialize radar signal processor
     radar_processor = RadarSignalProcessor(
-        num_chirps=args.num_chirps,
+        num_chirps=args.num_chirp,
         num_samples=args.num_samples,
         num_antennas=args.num_antennas,
         center_freq=args.center_freq,
@@ -168,18 +222,18 @@ def main():
     )
 
     # Receive data
-    data_to_process = np.zeros((args.num_chirps_process, args.num_samples, args.num_antennas))
+    data_to_process = np.zeros((args.num_chirp, args.num_samples, args.num_antennas))
     i = 0
     while True:
-        data = tcp_server.receive_data(args.num_chirps * args.num_samples * args.num_antennas * 4)
+        data = tcp_server.receive_data(args.num_chirps_per_frame * args.num_samples * args.num_antennas * 4)
         if not data:
             break
         data = np.frombuffer(data, dtype=np.float32)
-        data = data.reshape(args.num_chirps, args.num_samples, args.num_antennas)
-        data_to_process[i:i+args.num_chirps, :, :] = data
+        data = data.reshape(args.num_chirps_per_frame, args.num_samples, args.num_antennas)
+        data_to_process[i:i+args.num_chirps_per_frame, :, :] = data
 
-        i += args.num_chirps
-        if i == args.num_chirps_process:
+        i += args.num_chirps_per_frame
+        if i == args.num_chirp:
             # Process radar signal
             points = radar_processor.process_signal(data_to_process)
             filename = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S.png')
