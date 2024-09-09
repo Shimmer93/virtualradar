f9806a7a44d3e7c3bedc7fed92753e2b5a9b436c
Shimmer93 <zpengac@connect.ust.hk>
2024-09-10 01:05:15 +0800

    unfinished better processing

--- ProcessingScripts/main.py
+++ ProcessingScripts/main.py
@@ -8,6 +8,7 @@ from scipy.linalg import eigh
 import matplotlib.pyplot as plt
 import argparse
 import datetime
+import os
 
 class TCPServer:
     def __init__(self, host, port, timeout=120):
@@ -46,6 +47,7 @@ class RadarSignalProcessor:
                  cfar_num_train=8,
                  cfar_num_guard=4,
                  cfar_false_alarm_rate=1e-6,
+                 use_window=True
                 ):
         self.num_chirps = num_chirps
         self.num_samples = num_samples
@@ -61,13 +63,13 @@ class RadarSignalProcessor:
         self.range_res = c / (2 * self.bandwidth) # Range resolution, unit: m
         self.doppler_res = c * (1 / (self.num_chirps * self.dur_chirp)) / self.center_freq # Doppler speed resolution, unit: m/s
 
-        self.cfar_num_train = cfar_num_train // 2
-        self.cfar_num_guard = cfar_num_guard // 2
+        self.cfar_num_train = cfar_num_train
+        self.cfar_num_guard = cfar_num_guard
         self.cfar_threshold = cfar_num_train * (cfar_false_alarm_rate ** (-1 / cfar_num_train) - 1)
 
         self.win_range = hann(self.num_samples)
         self.win_doppler = hann(self.num_chirps)
-        self.use_window = True
+        self.use_window = use_window
 
     def range_fft(self, data):
         if self.use_window:
@@ -84,10 +86,13 @@ class RadarSignalProcessor:
     def cfar(self, data):
         # data: num_chirps x num_samples
         assert data.shape[1] > self.cfar_num_train + self.cfar_num_guard, 'Signal length is too short'
+        num_train_half = self.cfar_num_train // 2
+        num_guard_half = self.cfar_num_guard // 2
+        num_all_half = num_train_half + num_guard_half
         cfar_map = np.zeros_like(data)
         for i in range(data.shape[0]):
-            for j in range(self.cfar_num_train + self.cfar_num_guard, data.shape[1] - self.cfar_num_train - self.cfar_num_guard):
-                training_cells = np.concatenate([data[i, j - self.cfar_num_train - self.cfar_num_guard:j - self.cfar_num_guard], data[i, j + self.cfar_num_guard + 1:j + self.cfar_num_guard + self.cfar_num_train + 1]])
+            for j in range(num_all_half, data.shape[1] - num_all_half):
+                training_cells = np.concatenate([data[i, j - num_all_half:j - num_guard_half], data[i, j + num_guard_half + 1:j + num_all_half + 1]])
                 noise_level = np.mean(training_cells)
                 threshold = self.cfar_threshold * noise_level
                 if data[i, j] > threshold:
@@ -111,6 +116,12 @@ class RadarSignalProcessor:
     #     return cfar_map
     
     def process_signal(self, data):
+        # TODOs:
+        # 1. more antennas (12 should be enough)
+        # 2. beamforming
+        # 3. clutter removal
+        # 4. stap (space-time adaptive processing)
+        # 5. multiple targets
         # data: num_chirps x num_samples x num_antennas
         # assert data.shape == (self.num_chirps, self.num_samples, self.num_antennas), f'Data shape is incorrect: {data.shape}'
 
@@ -161,6 +172,8 @@ class RadarSignalProcessor:
         # Convert to numpy array
         if len(points) > 0:
             points = np.stack(points)
+            filename = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S.png')
+            plot_points(points, filename=filename)
             print(points.shape)
         else:
             points = None
@@ -168,13 +181,16 @@ class RadarSignalProcessor:
 
         return points
     
-def plot_range_doppler(doppler_fft, filename='range_doppler.png'):
-    if doppler_fft is None:
+def plot_range_doppler(range_doppler_map, cfar_map, filename='range_doppler.png'):
+    if range_doppler_map is None:
         return
-    fig, ax = plt.subplots()
-    ax.imshow(doppler_fft, aspect='auto', extent=[0, doppler_fft.shape[1], -doppler_fft.shape[0]//2, doppler_fft.shape[0]//2])
-    ax.set_xlabel('Range Bins')
-    ax.set_ylabel('Doppler Bins')
+    fig = plt.figure()
+    ax_rd = fig.add_subplot(121)
+    ax_rd.imshow(range_doppler_map, aspect='auto', extent=[0, range_doppler_map.shape[1], 0, range_doppler_map.shape[0]])
+    ax_rd.set_xlabel('Range Bins')
+    ax_rd.set_ylabel('Doppler Bins')
+    ax_rd.set_title('Range-Doppler Map')
+
     fig.savefig(filename)
 
 def plot_points(points, filename='points.png'):
@@ -201,9 +217,15 @@ def main():
     parser.add_argument('--sample_freq', type=float, default=300000, help='Sampling frequency')
     parser.add_argument('--bandwidth', type=float, default=7e9, help='Bandwidth')
     parser.add_argument('--dist_antenna', type=float, default=-1, help='Distance between antennas')
-    parser.add_argument('--cfar_num_train', type=int, default=8, help='Number of training cells')
-    parser.add_argument('--cfar_num_guard', type=int, default=2, help='Number of guard cells')
-    parser.add_argument('--cfar_false_alarm_rate', type=float, default=1e-4, help='False alarm rate')
+    parser.add_argument('--cfar_num_train', type=int, default=16, help='Number of training cells')
+    parser.add_argument('--cfar_num_guard', type=int, default=4, help='Number of guard cells')
+    parser.add_argument('--cfar_false_alarm_rate', type=float, default=1e-2, help='False alarm rate')
+    parser.add_argument('--use_window', type=bool, default=True, help='Use window function')
+    parser.add_argument('--save_raw_data', type=bool, default=False, help='Save raw data')
+    parser.add_argument('--save_pointcloud', type=bool, default=False, help='Save point cloud')
+    parser.add_argument('--vis_range_doppler', type=bool, default=True, help='Visualize range-doppler map')
+    parser.add_argument('--vis_pointcloud', type=bool, default=True, help='Visualize point cloud')
+    parser.add_argument('--output_dir', type=str, default='ReceivedData', help='Save directory')
     args = parser.parse_args()
 
     # Initialize TCP server
@@ -222,11 +244,18 @@ def main():
         cfar_num_train=args.cfar_num_train,
         cfar_num_guard=args.cfar_num_guard,
         cfar_false_alarm_rate=args.cfar_false_alarm_rate,
+        use_window=args.use_window
     )
 
     # Receive data
     data_to_process = np.zeros((args.num_chirps, args.num_samples, args.num_antennas))
     i = 0
+    idx_frame = 0
+    data_dir = f'{args.save_dir}/{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
+    os.makedirs(data_dir, exist_ok=True)
+    os.makedirs(f'{data_dir}/raw', exist_ok=True)
+    os.makedirs(f'{data_dir}/pointcloud', exist_ok=True)
+
     while True:
         data = tcp_server.receive_data(args.num_chirps_per_frame * args.num_samples * args.num_antennas * 4)
         if not data:
@@ -238,10 +267,10 @@ def main():
         i += args.num_chirps_per_frame
         if i == args.num_chirps:
             # Process radar signal
+            np.save(f'{data_dir}/{idx_frame:03d}.npy', data_to_process)
             points = radar_processor.process_signal(data_to_process)
-            filename = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S.png')
-            plot_points(points, filename=filename)
             i = 0
+            idx_frame += 1
 
     # Close TCP server
     tcp_server.close_server()
